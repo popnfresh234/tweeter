a42a143db199d648fb14e1ef1d97ef7371d59f04
Alex <ajollid@gmail.com>
2018-03-27 14:16:00 -0700

    Uses hardocded array of tweets, implements renderTweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- const tweetData = {
+// Fake data taken from tweets.json
+const data = [
+{
   "user": {
     "name": "Newton",
     "avatars": {
@@ -18,20 +20,54 @@
     "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
-};
+},
+{
+  "user": {
+    "name": "Descartes",
+    "avatars": {
+      "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+      "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+      "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+    },
+    "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+  ];
+
 
 function createTweetElement(tweet){
-  let $tweet = $("<article>").addClass("tweet-article");
+  var $tweet = $('<article>').addClass('tweet-article');
 
   //build header
-  let header = $("<header>").addClass("tweet-header");
+  var header = $('<header>').addClass('tweet-header');
   header.append('<img src="' + tweet.user.avatars.small + '"/>');
   header.append('<h4>'+tweet.user.name +'</h4>');
   header.append('<h6>' +tweet.user.handle + '</h6>');
   $tweet.append(header);
+
+  //add content
   $tweet.append('<p>' + tweet.content.text + '</p>');
 
-  let footer = $("<footer>").addClass("tweet-footer");
+  //build and append
+  var footer = $('<footer>').addClass('tweet-footer');
   footer.append('<p>10 days ago</p>');
   footer.append('<i class="fas fa-heart icons"></i>');
   footer.append('<i class="fas fa-retweet icons"></i>');
@@ -41,10 +77,14 @@ function createTweetElement(tweet){
   return $tweet;
 }
 
-
+function renderTweets(tweets){
+  tweets.forEach(function(tweet){
+    var $tweet = createTweetElement(tweet);
+    $('#tweets-section').append($tweet);
+  });
+}
 
 // Test / driver code (temporary)
 $(document).ready(function(){
-  var $tweet = createTweetElement(tweetData);
-  $('#tweets-section').append($tweet);
+  renderTweets(data);
 });
