f09b3d7f10482ea482ed912826c9e5d088ad2f55
Alex <ajollid@gmail.com>
2018-03-27 18:05:00 -0700

    Adds input validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,25 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function createTweetElement(tweet){
+ function createTweetElement(tweet){
   var $tweet = $('<article>').addClass('tweet-article');
 
   //build header
-  var header = $('<header>').addClass('tweet-header');
-  header.append('<img src="' + tweet.user.avatars.small + '"/>');
-  header.append('<h4>'+tweet.user.name +'</h4>');
-  header.append('<h6>' +tweet.user.handle + '</h6>');
+  var header = $('<header>').addClass('tweet-header')
+  .append('<img src="' + tweet.user.avatars.small + '"/>')
+  .append('<h4>'+tweet.user.name +'</h4>')
+  .append('<h6>' +tweet.user.handle + '</h6>');
+
   $tweet.append(header);
 
   //add content
   $tweet.append('<p>' + tweet.content.text + '</p>');
 
   //build and append
-  var footer = $('<footer>').addClass('tweet-footer');
-  footer.append('<p>10 days ago</p>');
-  footer.append('<i class="fas fa-heart icons"></i>');
-  footer.append('<i class="fas fa-retweet icons"></i>');
-  footer.append('<i class="fas fa-flag icons"></i>');
+  var footer = $('<footer>').addClass('tweet-footer')
+  .append('<p>10 days ago</p>')
+  .append('<i class="fas fa-heart icons"></i>')
+  .append('<i class="fas fa-retweet icons"></i>')
+  .append('<i class="fas fa-flag icons"></i>');
+
   $tweet.append(footer);
 
   return $tweet;
@@ -45,11 +47,26 @@ function loadTweets () {
   });
 }
 
+function handleTweet(input){
+  var errorMsg = $('.tweet-error');
+  if ( input.length > 140 ) {
+    $(errorMsg).text('Tweet is too long!');
+    $(errorMsg).css('visibility', 'visible');
+  }else if( input === "" ) {
+    $(errorMsg).text('Empty tweet');
+    $(errorMsg).css('visibility', 'visible');
+  } else {
+    errorMsg.css('visibility', 'hidden');
+    console.log('Submit!');
+  }
+}
+
 // Test / driver code (temporary)
 $(document).ready( function () {
   loadTweets();
+  //Handle new tweets
   $('.new-tweet').find('input').on('click', function (event) {
     event.preventDefault();
-    console.log($(this).parent().serialize());
+    handleTweet($(this).parent().serialize().split('=')[1]);
   });
 });
