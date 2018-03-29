768bcd78476512a5b0e32d0d04d9bcbb20f816f9
Alex Holliday <ajhollid@gmail.com>
2018-03-29 06:03:28 +0000

    Refactors methods into ready function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,95 +4,100 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
 
- function createTweetElement(tweet){
-  var $tweet = $('<article data-id="' + tweet._id + '">').addClass('tweet-article');
 
-  //build header
-  var header = $('<header>').addClass('tweet-header')
-  .append('<img src="' + tweet.user.avatars.small + '"/>')
-  .append('<h4>' + tweet.user.name +'</h4>')
-  .append('<h6>' + tweet.user.handle + '</h6>');
 
-  $tweet.append(header);
-
-  //add content
-  $tweet.append('<p>' + escape(tweet.content.text) + '</p>');
-
-  //build and append
-  var footer = $('<footer>').addClass('tweet-footer')
-  .append('<p>' + moment(tweet.created_at).fromNow() + '</p>')
-  .append('<span class="icons like-counter">0</span>')
-  .append('<span class="heart"><i class="fas fa-heart icons"></i></span>')
-  .append('<span><i class="fas fa-retweet icons"></i></span>')
-  .append('<span><i class="fas fa-flag icons"></i></span>');
-
-  $tweet.append(footer);
-
-  return $tweet;
-}
-
-function renderTweets (tweets) {
-  tweets.forEach((tweet) => {
-    var $tweet = createTweetElement (tweet);
-    $('#tweets-section').prepend ($tweet);
-  });
-}
-
-function loadTweets () {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    success: function (jsonTweets) {
-      renderTweets(jsonTweets);
-    }
-  });
-}
-
-function postTweet (tweet) {
-  $.ajax({
-    url: '/tweets',
-    method: 'POST',
-    data: tweet,
-    dataType: 'text',
-    success: function (){
-     loadTweets();
-    }
-  });
-}
-
-function handleTweet(tweet){
-  var input = tweet.split('=')[1];
-  var errorMsg = $('.tweet-error');
-  if ( input.length > 140 ) {
-    $(errorMsg).text('Tweet is too long!');
-    $(errorMsg).css('visibility', 'visible');
-  } else if ( input === "" ) {
-    $(errorMsg).text('Empty tweet');
-    $(errorMsg).css('visibility', 'visible');
-  } else {
-    errorMsg.css('visibility', 'hidden');
-    postTweet(tweet);
-  }
-}
-
-// Test / driver code (temporary)
 $( function () {
   var KEY_LIKED = "liked";
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  function createTweetElement(tweet){
+    var $tweet = $('<article data-id="' + tweet._id + '">').addClass('tweet-article');
+
+    //build header
+    var header = $('<header>').addClass('tweet-header')
+    .append('<img src="' + tweet.user.avatars.small + '"/>')
+    .append('<h4>' + tweet.user.name +'</h4>')
+    .append('<h6>' + tweet.user.handle + '</h6>');
+
+    $tweet.append(header);
+
+    //add content
+    $tweet.append('<p>' + escape(tweet.content.text) + '</p>');
+
+    //build and append
+    var footer = $('<footer>').addClass('tweet-footer')
+    .append('<p>' + moment(tweet.created_at).fromNow() + '</p>')
+    .append('<span class="icons like-counter">0</span>')
+    .append('<span class="heart"><i class="fas fa-heart icons"></i></span>')
+    .append('<span><i class="fas fa-retweet icons"></i></span>')
+    .append('<span><i class="fas fa-flag icons"></i></span>');
+
+    $tweet.append(footer);
+
+    return $tweet;
+  }
+
+  function renderTweets (tweets) {
+    tweets.forEach((tweet) => {
+      var $tweet = createTweetElement (tweet);
+      $('#tweets-section').prepend ($tweet);
+    });
+  }
+
+  function loadTweets () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (jsonTweets) {
+        renderTweets(jsonTweets);
+      }
+    });
+  }
+
+  function postTweet (tweet) {
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: tweet,
+      dataType: 'text',
+      success: function (){
+       loadTweets();
+      }
+    });
+  }
+
+  function handleTweet(tweet){
+    var input = tweet.split('=')[1];
+    var errorMsg = $('.tweet-error');
+    if ( input.length > 140 ) {
+      $(errorMsg).text('Tweet is too long!');
+      $(errorMsg).css('visibility', 'visible');
+    } else if ( input === "" ) {
+      $(errorMsg).text('Empty tweet');
+      $(errorMsg).css('visibility', 'visible');
+    } else {
+      errorMsg.css('visibility', 'hidden');
+      postTweet(tweet);
+    }
+  }
+
+
+  //Initial load
   loadTweets();
-  console.log(moment().startOf('hour').fromNow());
+
   //Handle new tweets
   $('.new-tweet').find('input').on('click', function (event) {
     event.preventDefault();
     handleTweet($(this).parent().serialize());
   });
 
+  //Handle clickingon menu items
   $('a').on('click', function (event) {
     event.preventDefault();
     var id = $(this).attr('href');
@@ -105,7 +110,9 @@ $( function () {
 
   //Listen for delegated event on tweets section
   $('#tweets-section').on('click', '.heart', function(event){
+    //Get post ID
     var id = $(event.target).closest('article').data();
+
     var likes = 0;
     var counter = $(this).prev();
     if($(this).data(KEY_LIKED)){
