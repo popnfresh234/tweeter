352aa9f096c16d459bd4869b235cd28f2265360a
Alex Holliday <ajhollid@gmail.com>
2018-03-30 01:27:51 +0000

    Updats port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
